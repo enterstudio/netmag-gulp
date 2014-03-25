501214ce488261db2f641a7ff03c7340b7c6e4e4
Dan Reeves <heydanreeves@gmail.com>
2014-03-25 14:06:39 +0000

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+
+var compass = require('gulp-compass');
+var minifyCSS = require('gulp-minify-css');
+var express = require('express');
+var livereload = require('gulp-livereload');
+
+gulp.task('compass', function() {
+    gulp.src('./assets/css/*.scss')
+        .pipe(compass({
+            config_file: './compass/config.rb',
+            css: 'stylesheets',
+            sass: 'sass'
+        }))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('./assets/css/'))
+        .pipe(livereload());
+});
+
+gulp.task('serve', function () {
+    var app = express();
+    app.use(express.static(__dirname));
+    app.listen(1337);
+});
+
+
+gulp.task('watch', function () {
+    gulp.watch(['./assets/css/*.scss'], ['compass']);
+});
+
+gulp.task('default', ['compass', 'serve', 'watch']);
